84bb8bf20d0dfe73cb70b5581f960f56c492a743
phillias <phillias@gmail.com>
2026-05-18 20:50:49 -0400

    Fix Cilium CNI chaining, node scaling, and ingress-nginx config (#5)
    Co-authored-by: darren.slocum@gmail.com <darren.slocumgmail.com@DarrenSlocum-Mac.local>

--- terraform/helm.tf
+++ terraform/helm.tf
@@ -17,7 +17,7 @@ locals {
       configMap     = "cni-configuration"
       customConf    = true
       exclusive     = false
-      install       = false
+      install       = true
     }
     nodeinit = {
       enabled = true
@@ -70,12 +70,14 @@ resource "helm_release" "cert_manager" {
   chart            = "cert-manager"
   namespace        = "cert-manager"
   create_namespace = true
-  version          = "~> 1.16"
+  version          = "1.16.5"
 
-  set {
-    name  = "installCRDs"
-    value = "true"
-  }
+  set = [
+    {
+      name  = "installCRDs"
+      value = "true"
+    }
+  ]
 }
 
 # ── ingress-nginx (default) ───────────────────────────────────────────────────
@@ -85,12 +87,19 @@ resource "helm_release" "ingress_nginx" {
   chart            = "ingress-nginx"
   namespace        = "ingress-nginx"
   create_namespace = true
-  version          = "~> 4.15"
+  version          = "4.15.1"
+  timeout          = 600
 
-  set {
-    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/azure-load-balancer-health-probe-request-path"
-    value = "/healthz"
-  }
+  set = [
+    {
+      name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/azure-load-balancer-health-probe-request-path"
+      value = "/healthz"
+    },
+    {
+      name  = "controller.admissionWebhooks.enabled"
+      value = "false"
+    }
+  ]
 
   depends_on = [
     helm_release.cert_manager
@@ -104,7 +113,7 @@ resource "helm_release" "cilium" {
   chart            = "cilium"
   namespace        = "cilium"
   create_namespace = true
-  version          = "~> 1.19"
+  version          = "1.19.4"
 
   values = [
     yamlencode(local.cilium_values)
@@ -122,36 +131,38 @@ resource "helm_release" "cilium_ingress_nginx" {
   chart            = "ingress-nginx"
   namespace        = "cilium-ingress-nginx"
   create_namespace = true
-  version          = "~> 4.15"
+  version          = "4.15.1"
 
-  set {
-    name  = "controller.ingressClassResource.name"
-    value = "cilium-nginx"
-  }
-  set {
-    name  = "controller.ingressClassResource.controllerValue"
-    value = "k8s.io/cilium-ingress-nginx"
-  }
-  set {
-    name  = "controller.ingressClass"
-    value = "cilium-nginx"
-  }
-  set {
-    name  = "controller.ingressClassByName"
-    value = "true"
-  }
-  set {
-    name  = "controller.watchIngressWithoutClass"
-    value = "false"
-  }
-  set {
-    name  = "controller.service.externalTrafficPolicy"
-    value = "Local"
-  }
-  set {
-    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/azure-load-balancer-health-probe-request-path"
-    value = "/healthz"
-  }
+  set = [
+    {
+      name  = "controller.ingressClassResource.name"
+      value = "cilium-nginx"
+    },
+    {
+      name  = "controller.ingressClassResource.controllerValue"
+      value = "k8s.io/cilium-ingress-nginx"
+    },
+    {
+      name  = "controller.ingressClass"
+      value = "cilium-nginx"
+    },
+    {
+      name  = "controller.ingressClassByName"
+      value = "true"
+    },
+    {
+      name  = "controller.watchIngressWithoutClass"
+      value = "false"
+    },
+    {
+      name  = "controller.service.externalTrafficPolicy"
+      value = "Local"
+    },
+    {
+      name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/azure-load-balancer-health-probe-request-path"
+      value = "/healthz"
+    }
+  ]
 
   depends_on = [
     helm_release.cilium,
@@ -166,24 +177,26 @@ resource "helm_release" "echo_server_nginx" {
   chart            = "echo-server"
   namespace        = "echo-server-nginx"
   create_namespace = true
-  version          = "~> 0.5"
+  version          = "0.5.0"
 
-  set {
-    name  = "tolerations[0].effect"
-    value = "NoSchedule"
-  }
-  set {
-    name  = "tolerations[0].key"
-    value = "kubernetes.azure.com/scalesetpriority"
-  }
-  set {
-    name  = "tolerations[0].operator"
-    value = "Equal"
-  }
-  set {
-    name  = "tolerations[0].value"
-    value = "spot"
-  }
+  set = [
+    {
+      name  = "tolerations[0].effect"
+      value = "NoSchedule"
+    },
+    {
+      name  = "tolerations[0].key"
+      value = "kubernetes.azure.com/scalesetpriority"
+    },
+    {
+      name  = "tolerations[0].operator"
+      value = "Equal"
+    },
+    {
+      name  = "tolerations[0].value"
+      value = "spot"
+    }
+  ]
 
   depends_on = [
     helm_release.ingress_nginx
@@ -197,24 +210,26 @@ resource "helm_release" "echo_server_cilium" {
   chart            = "echo-server"
   namespace        = "echo-server-cilium"
   create_namespace = true
-  version          = "~> 0.5"
+  version          = "0.5.0"
 
-  set {
-    name  = "tolerations[0].effect"
-    value = "NoSchedule"
-  }
-  set {
-    name  = "tolerations[0].key"
-    value = "kubernetes.azure.com/scalesetpriority"
-  }
-  set {
-    name  = "tolerations[0].operator"
-    value = "Equal"
-  }
-  set {
-    name  = "tolerations[0].value"
-    value = "spot"
-  }
+  set = [
+    {
+      name  = "tolerations[0].effect"
+      value = "NoSchedule"
+    },
+    {
+      name  = "tolerations[0].key"
+      value = "kubernetes.azure.com/scalesetpriority"
+    },
+    {
+      name  = "tolerations[0].operator"
+      value = "Equal"
+    },
+    {
+      name  = "tolerations[0].value"
+      value = "spot"
+    }
+  ]
 
   depends_on = [
     helm_release.cilium_ingress_nginx
@@ -228,10 +243,12 @@ resource "helm_release" "kubeview" {
   namespace        = "kubeview"
   create_namespace = true
 
-  set {
-    name  = "loadBalancer.enabled"
-    value = "true"
-  }
+  set = [
+    {
+      name  = "loadBalancer.enabled"
+      value = "true"
+    }
+  ]
 
   depends_on = [
     helm_release.cert_manager
